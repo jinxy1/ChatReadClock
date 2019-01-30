d76981ff8550864bfd644ac29edc59e3fecac49c
jinxy <17611268055@163.com>
2019-01-30 15:34:26 +0800

    进晓宇 20190130

--- app-abd/src/com/asiainfo/abdinfo/service/PlanDaoService.java
+++ app-abd/src/com/asiainfo/abdinfo/service/PlanDaoService.java
@@ -1,8 +1,11 @@
 package com.asiainfo.abdinfo.service;
 
 import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Param;
 import com.asiainfo.abdinfo.po.DayPlan;
+import com.asiainfo.abdinfo.po.PlanData;
 import com.asiainfo.abdinfo.po.PlanRecord;
 
 /**
@@ -15,4 +18,19 @@ public interface PlanDaoService {
 	List<DayPlan> makePlan(@Param("staffCode") String staffCode);
 	
 	void insertPlanRecord( String staffCode,String planTime, List<PlanRecord> list);
+	
+	/**
+	 * 反查插入的计划
+	 * @param staffCode
+	 * @param planTime
+	 * @return
+	 */
+	List<PlanData> selectPlan(@Param(value="staffCode") String staffCode,@Param(value="planTime") String planTime);
+	
+	
+	/**
+ 	 * 反查删除的数据
+ 	 * @param id
+ 	 */
+	void delectPlan(String id);
 }
--- app-abd/src/com/asiainfo/abdinfo/test/TestDate.java
+++ app-abd/src/com/asiainfo/abdinfo/test/TestDate.java
@@ -1,13 +1,7 @@
 package com.asiainfo.abdinfo.test;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
+
+
 import java.util.List;
 import java.util.Map;
 
@@ -15,40 +9,20 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.asiainfo.abdinfo.po.BaseInformation;
-import com.asiainfo.abdinfo.service.impl.NewLoginImple;
+import com.asiainfo.abdinfo.po.PlanData;
 import com.asiainfo.abdinfo.service.impl.PlanDaoServiceImple;
-import com.asiainfo.abdinfo.service.impl.ReadClockImple;
-import com.asiainfo.abdinfo.service.impl.SixDiligenceServiceImpl;
 
 public class TestDate {
 
 	@Test
-	public void checser() {
-		
-	}
-
-	@Test
-	public void test2() {
-	
-	}
-
-	@Test
-	public void test3() {
-		
-	}
-
-	
-	@Test
-	public void test4() {
+	public void test() {
 		PlanDaoServiceImple mlcvp;
 		ApplicationContext ac = new ClassPathXmlApplicationContext("conf/spring-mvc.xml", "conf/spring-mybatis.xml");
 		mlcvp = ac.getBean("PlanDaoService", PlanDaoServiceImple.class);
 	    String staffCode="18060405";
-	    String planTime="2019-01-28";
-	   // [{"type":"系统研发","projectName":"微信小程序开发","timeLong":0.3,"planContent":"fdf"},{"type":"系统研发","projectName":"微信小程序开发","timeLong":0.3,"planContent":"fd"}]
-	    
-	//	mlcvp.insertPlanRecord(staffCode, planTime, list);
+	    String planTime="2019-01-29";
+	    List<PlanData>map=mlcvp.selectPlan(staffCode, planTime);
+	    System.out.println(map);
 		
 	
 	}
